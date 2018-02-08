32b3be1c5ed3e8ff7a8c378e89b249926dd1b61a
Hani <Hani@Hanis-MBP.local>
2018-02-09 02:50:16 +0530

    Just a test push

--- app/src/main/java/com/mytaxi/android_demo/App.java
+++ app/src/main/java/com/mytaxi/android_demo/App.java
@@ -11,6 +11,7 @@ public class App extends Application {
 
     private AppComponent mAppComponent;
 
+
     public static App getApplicationContext(Context context) {
         return (App) context.getApplicationContext();
     }
